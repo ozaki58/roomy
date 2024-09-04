0ac5fad19e44789f4950534027705eb239d03f2c
ozaki <ozaki@7659sw.com>
2024-09-04 11:47:23 +0900

    threadDataをjsonで扱うように変更

--- app/group/[name]/page.tsx
+++ app/group/[name]/page.tsx
@@ -4,33 +4,14 @@ import React, { useState } from 'react';
 import { Thread } from '@/components/types';  // types.ts からインポート
 import { TextareaForm } from '@/components/textareaForm';
 import ThreadList from '@/components/threadList';
+import threadData from '@/data/threadData.json';
 export default function GroupPage() {
     const params = useParams();  // URLから動的パラメータを取得
     const encodedGroupName = Array.isArray(params.name) ? params.name[0] : params.name;  // 配列の場合は最初の要素を取得
     const groupName = decodeURIComponent(encodedGroupName);  // デコードして読みやすい形に変換
-    const [threadsData,setThreadsData] = useState<Thread[]>( [
-        {
-          author: "ひらりん@",
-          date: "2024年8月17日 午前12:34",
-          content: "皆さん、こんばんは。実はうちのチームのメンバーが...",
-          reactions: "👍😢",
-          reactionCount: 6,
-          commentCount: 4,
-          shareCount: 1,
-          comments: [
-            {
-              author: "ひらりん@",
-              content: "結局またアカウントは凍結されたままで復帰出来ずに難航してるみたいです😢遠法だから認められないの一点張りみたいです。",
-              date: "8月27日 午前12:49"
-            },
-            {
-              author: "s819ひーし",
-              content: "😭😭😭私のSERVERも bot altが処分されたのですが その原因が私のせいにされて　　SERVERでの私の居場所がなくなりました。...",
-              date: "8月27日 午後7:49"
-            },
-          ],
-        },
-      ])
+    const [threadsData,setThreadsData] = useState<Thread[]>( 
+        threadData.threadData
+      )
     
       const addThread = (newThread: Thread) => {
         setThreadsData([newThread, ...threadsData]) // 新しいスレッドを先頭に追加
