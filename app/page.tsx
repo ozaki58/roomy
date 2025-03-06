79f57eb4c1231abf51e758ece8cfd5b2fae195db
ozaki <ozaki@7659sw.com>
2025-03-06 14:57:31 +0900

    ルートページに到達した際はhomeルートに遷移するように変更

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,8 @@
-"use client"; // これを追加
+// app/page.tsx
+'use client';
+import { redirect } from 'next/navigation';
 
-import Header from "@/components/header";
-import Image from "next/image";
-
-import ThreadList from '@/components/threadList'
-
-import { TextareaForm } from "@/components/textareaForm";
-import BottomNavigation from "@/components/bottom-navigation";
-import React,{useState} from "react"
-import { Thread } from "@/components/types"  // types.ts からインポート
-import Sidebar from "@/components/sidebar";
 export default function App() {
-  
-  const [threadsData,setThreadsData] = useState<Thread[]>( 
-    []
-  )
-
-  const addThread = (newThread: Thread) => {
-    setThreadsData([newThread, ...threadsData]) // 新しいスレッドを先頭に追加
-  }
-
-  return (
-    <>
-    
-    <TextareaForm addThread={addThread} />
-    <ThreadList threads={threadsData} />
-    
-    
-    </>
-    
-  )
+  redirect('/home');
+  return null;
 }
